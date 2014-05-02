ece655ed47864c30092743b172dad0bced565514
fukayatsu <fukayatsu@gmail.com>
2014-05-03 00:50:34 +0900

    Add lita config

--- Gemfile
+++ Gemfile
@@ -3,14 +3,4 @@ source "https://rubygems.org"
 gem "lita"
 gem 'lita-twitter'
 gem 'lita-pebbles'
-
-# Uncomment to use the HipChat adapter
-# gem "lita-hipchat"
-
-# Uncomment to use the IRC adapter
-# gem "lita-irc"
-
-# Add handlers to give Lita new functionality.
-# For example:
-# gem "lita-google-images"
-# gem "lita-karma"
+gem "lita-google-images"
--- lita_config.rb
+++ lita_config.rb
@@ -1,33 +1,13 @@
 Lita.configure do |config|
-  # The name your robot will use.
-  config.robot.name = "Lita"
+  config.robot.name       = ENV["BOT_NAME"] || 'lita'
+  config.robot.adapter    = ENV["ADAPTER"] ? ENV["ADAPTER"].to_sym : :shell
+  config.robot.log_level  = :info
 
-  # The locale code for the language to use.
-  # config.robot.locale = :en
+  config.redis.url = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["PORT"]
 
-  # The severity of messages to log. Options are:
-  # :debug, :info, :warn, :error, :fatal
-  # Messages at the selected level and above will be logged.
-  config.robot.log_level = :info
-
-  # An array of user IDs that are considered administrators. These users
-  # the ability to add and remove other users from authorization groups.
-  # What is considered a user ID will change depending on which adapter you use.
-  # config.robot.admins = ["1", "2"]
-
-  # The adapter you want to connect with. Make sure you've added the
-  # appropriate gem to the Gemfile.
-  config.robot.adapter = :shell
-
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
-
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
-
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  config.adapter.api_key             = ENV['API_KEY']
+  config.adapter.api_secret          = ENV['API_SECRET']
+  config.adapter.access_token        = ENV['ACCESS_TOKEN']
+  config.adapter.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
