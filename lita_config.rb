6dcc136ede20a480cb44b113b4e5d3449f4362e4
fukayatsu <fukayatsu@gmail.com>
2015-04-16 00:42:52 +0900

    Bump version of lita-talk

--- lita_config.rb
+++ lita_config.rb
@@ -22,5 +22,5 @@ Lita.configure do |config|
   config.handlers.schedules.should_wake_up_at = '0 8 * * * Asia/Tokyo'
 
   config.handlers.talk.docomo_api_key      = ENV['DOCOMO_API_KEY']
-  config.handlers.talk.docomo_character_id = 20
+  config.handlers.talk.docomo_character_id = [nil, 20, 30]
 end
