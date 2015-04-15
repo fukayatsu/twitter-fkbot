297d3e70c960bc20a907ba127cdcb07573cd715f
fukayatsu <fukayatsu@gmail.com>
2015-04-15 18:06:08 +0900

    Introduce lita-talk

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 source 'https://rubygems.org'
 # source "https://s3.amazonaws.com/production.s3.rubygems.org"
 
-gem "lita",         '~> 3.2.0'
-gem 'lita-twitter', '0.0.6'
-gem 'lita-pebbles', '0.1.8'
+gem "lita"
+gem 'lita-twitter'
+gem 'lita-pebbles'
 gem "lita-google-images"
-gem "lita-sushiyuki", '0.0.6'
+gem "lita-sushiyuki"
+gem 'lita-talk'
 
 gem 'dotenv'
--- lita_config.rb
+++ lita_config.rb
@@ -8,15 +8,19 @@ Lita.configure do |config|
   config.robot.adapter    = ENV["ADAPTER"] ? ENV["ADAPTER"].to_sym : :shell
   config.robot.log_level  = :info
 
-  config.redis.url = ENV["REDISTOGO_URL"]
-  config.http.port = ENV["PORT"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["PORT"] || 3456
 
-  config.adapter.api_key             = ENV['API_KEY']
-  config.adapter.api_secret          = ENV['API_SECRET']
-  config.adapter.access_token        = ENV['ACCESS_TOKEN']
-  config.adapter.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+
+  config.adapters.twitter.api_key             = ENV['API_KEY']
+  config.adapters.twitter.api_secret          = ENV['API_SECRET']
+  config.adapters.twitter.access_token        = ENV['ACCESS_TOKEN']
+  config.adapters.twitter.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 
   config.handlers.schedules.room              = 1
   config.handlers.schedules.should_sleep_at   = '0 0 * * * Asia/Tokyo'
   config.handlers.schedules.should_wake_up_at = '0 8 * * * Asia/Tokyo'
+
+  config.handlers.talk.docomo_api_key      = ENV['DOCOMO_API_KEY']
+  config.handlers.talk.docomo_character_id = 20
 end
